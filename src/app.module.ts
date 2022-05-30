ad34e9557685cb76485c715a862460cfb6396fbe
juan-ossa <jcossax@gmail.com>
2022-05-30 08:52:23 -0500

    correcion crud 01 country

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,29 +7,29 @@ import { ActorModule } from './components/actor/actor.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'juan',
-      password: 'juan',
-      database: 'testorm',
-      entities: ['dist/entities/**/*.js'],
-      synchronize: false,
-    }),
-
     // TypeOrmModule.forRoot({
     //   type: 'postgres',
-    //   host: 'ec2-3-211-221-185.compute-1.amazonaws.com',
+    //   host: 'localhost',
     //   port: 5432,
-    //   username: 'coigjfmyvjvwex',
-    //   password:
-    //     '4edc0c7c180a5d4c6f320aab8183ffbdaf4b766f918c161d215eb764f4f3e925',
-    //   database: 'd6l7qtjpno9p65',
-    //   logging: false,
+    //   username: 'juan',
+    //   password: 'juan',
+    //   database: 'testorm',
     //   entities: ['dist/entities/**/*.js'],
     //   synchronize: false,
     // }),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-3-211-221-185.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'coigjfmyvjvwex',
+      password:
+        '4edc0c7c180a5d4c6f320aab8183ffbdaf4b766f918c161d215eb764f4f3e925',
+      database: 'd6l7qtjpno9p65',
+      logging: false,
+      entities: ['dist/entities/**/*.js'],
+      synchronize: false,
+    }),
 
     CountryModule,
 
